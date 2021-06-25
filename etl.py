da400164eb7e7029357c6f80eb0df0c5a7f4993a
jmoore87jr <base_1187@yahoo.com>
2021-06-25 15:28:57 -0700

    first commit

---
+++ etl.py
@@ -0,0 +1,18 @@
+import time
+import pandas as pd
+from sqlalchemy import create_engine
+import psycopg2
+from credentials import *
+
+def load_from_s3():
+    pass 
+
+def transform_dsams(df):
+    pass 
+
+def transform_cisil(df):
+    pass
+
+def upsert_to_postgres(df):
+    pass
+
---
+++ generate.py
@@ -0,0 +1,7 @@
+import pandas as pd 
+import numpy as np
+import random
+
+# generate initial data
+
+# generate new smaller data daily
