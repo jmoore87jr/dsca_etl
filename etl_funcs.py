13c841656ce67a753358ff3610970f76bb1f6e12
jmoore87jr <base_1187@yahoo.com>
2021-06-29 09:26:28 -0700

    separated into etl_funcs.py and main.py; deleted test file

--- etl_funcs.py
+++ etl_funcs.py
@@ -6,6 +6,8 @@ import boto3
 import s3fs
 from credentials import *
 
+# The original insert of 30M rows worked but it took hours...probably 8-12 hrs
+
 def to_s3(file, s3name):
     """upload a file to the s3 mnnk/dsca/ folder"""
     
@@ -14,13 +16,13 @@ def to_s3(file, s3name):
 
     print(f"{s3name} saved to S3")
 
-def from_s3(s3file, sep=','):
+def from_s3(s3file, cols, sep=','):
     """turn s3 file into pandas dataframe"""
     filetype = s3file[-3:]
 
     if filetype == 'csv':
         try:
-            df = pd.read_csv(s3file, sep=sep)
+            df = pd.read_csv(s3file, sep=sep, usecols=cols)
         except:
             print("ERROR")
     elif filetype == 'fwf':
@@ -31,30 +33,7 @@ def from_s3(s3file, sep=','):
     
     return df
 
-def create_table(df, table, conn, engine):
-    """
-    Create our table in Postgres
-    Change column names to match your data
-    """
-
-    engine.execute(f"DROP TABLE IF EXISTS {table}")
-
-    sql = f"""
-    CREATE TABLE {table} (
-        "ID" INTEGER UNIQUE,
-        "Item" VARCHAR,
-        "Date" VARCHAR,
-        "Date2" VARCHAR,
-        "Date3" VARCHAR
-    );
-    """
-
-    # execute upsert
-    engine.execute(sql)
-
-
-def initialize_postgres(df, table, pk):
-    """save pandas dataframe to a new postgres table with a primary key"""
+def connect_postgres():
     try:
         # connect to database
         conn = psycopg2.connect(
@@ -71,55 +50,79 @@ def initialize_postgres(df, table, pk):
 
         print("Engine created")
 
-        # insert data
-        try:
-            df.to_sql(table, con=engine, if_exists='replace', chunksize=20000)
-            print("Data inserted")
-        except:
-            print("Data format doesn't match database format")
-        
-        # make ID column PRIMARY KEY
-        engine.execute(f'ALTER TABLE {table} ADD CONSTRAINT "{pk}" PRIMARY KEY ("{pk}");')
-
-        print("ID altered to Primary Key")
-
-        # commit to database
-        conn.commit()
-        print("Table created")
-
-        # close connection
-        conn.close()
-        print("Connection closed")
-
     except psycopg2.OperationalError as e:
         print(e)
 
-def to_postgres(df, table):
-    """upsert pandas dataframe to postgres database"""
-    pass
+    return conn, engine
+
+def upsert_postgres(df, table, engine, colnames, pk):
+    """
+    "Merge" new data into existing Postgres database, 
+    replacing old data when a new row ID matches an
+    existing one
+    """
+
+    # Index(['ID', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'Z', 'Y', 'X', 'W', 'V', 'U', 'T', 'S', 'R', 'Q']
+
+    # string of colnames in quotes separated by comma and space
+    sql_colnames = ', '.join(list(map(lambda x: '"' + x + '"', colnames)))
+    sql_pk = '"' + pk + '"'
+    
+    # change column names to fit any DataFrame
+    sql = f""" 
+            INSERT INTO {table} ({sql_colnames})
+            VALUES {','.join([str(i) for i in list(df.to_records(index=False))])}
+            ON CONFLICT ({sql_pk})
+            DO  
+                UPDATE SET "ID" = EXCLUDED."ID",
+                           "A" = EXCLUDED."A",
+                           "B" = EXCLUDED."B",
+                           "C" = EXCLUDED."C",
+                           "D" = EXCLUDED."D",
+                           "E" = EXCLUDED."E",
+                           "F" = EXCLUDED."F",
+                           "G" = EXCLUDED."G",
+                           "H" = EXCLUDED."H",
+                           "I" = EXCLUDED."I",
+                           "J" = EXCLUDED."J",
+                           "Z" = EXCLUDED."Z",
+                           "Y" = EXCLUDED."Y",
+                           "X" = EXCLUDED."X",
+                           "W" = EXCLUDED."W",
+                           "V" = EXCLUDED."V",
+                           "U" = EXCLUDED."U",
+                           "T" = EXCLUDED."T",
+                           "S" = EXCLUDED."S",
+                           "R" = EXCLUDED."R",
+                           "Q" = EXCLUDED."Q";
+        """
+
+    # execute upsert
+    engine.execute(sql)
+
+def create_and_fill_table_postgres(engine, df, table):
+    """save pandas dataframe to a new postgres table with a primary key"""
+    # insert data
+    try:
+        start_time = time.time()
+        df.to_sql(table, con=engine, if_exists='replace', chunksize=20000, method='multi')
+        print(f"to_sql took {(time.time() - start_time)} seconds")
+        print(f"{table} created and filled")
+    except:
+        print("DataFrame format doesn't match database table format")
+
+def set_pk_postgres(conn, engine, table, pk):
+    """set primary key (otherwise upsert won't work)"""
+    
+    engine.execute(f'ALTER TABLE {table} ADD CONSTRAINT "{pk}" PRIMARY KEY ("{pk}");')
+
+    print(f"{pk} set to Primary Key")
 
 def merge(df1, df2, col1, col2):
-    """merge on ID column and time it"""
-    df = df1.merge(df2, left_on=col1, right_on=col2)
-    
-    return df
+    return df1.merge(df2, left_on=col1, right_on=col2)
 
 def sort(df, by, asc=True):
-    """sort by ID and time it"""
-    df = df.sort_values(by=by, ascending=asc)
+    return df.sort_values(by=by, ascending=asc)
 
-    return df
-
-
-df1 = pd.read_csv('s3://mnnk/dsca/numbers1_30000000.csv')
-df2 = pd.read_csv('s3://mnnk/dsca/numbers2_30000000.csv')
-
-df = merge(df1, df2, "ID", "ID")
-df = sort(df, by="ID", asc=True)
-
-print(df.head())
-print(df.columns)
-
-initialize_postgres(df, table="table1", pk="ID")
 
 
--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import time
 import pandas as pd
+from sqlalchemy import create_engine
+import psycopg2
+import boto3
+import s3fs
 from prefect import Flow
+from credentials import *
 from generate import generate_daily_data
-from etl_funcs import to_s3, from_s3, to_postgres, merge, sort
+from etl_funcs import to_s3, from_s3, connect_postgres, upsert_postgres, create_and_fill_table_postgres, set_pk_postgres, merge, sort
 
 """
 1. generate new daily data (already merged into final postgres format)
@@ -9,3 +15,24 @@ from etl_funcs import to_s3, from_s3, to_postgres, merge, sort
 3. pull from s3 and sort
 4. upsert to postgres
 """
+
+if __name__ == "__main__":
+
+    colnames = ['ID', 'A', 'B', 'C', 'D', 'E', 'F', 
+                'G', 'H', 'I', 'J', 'Z', 'Y', 'X', 
+                'W', 'V', 'U', 'T', 'S', 'R', 'Q']
+
+    df = from_s3(f'{S3_PATH}/new_data_2021-06-29.csv', cols=colnames)
+
+    conn, engine = connect_postgres() # connect to postgres database
+
+    #create_and_fill_table_postgres(engine=engine, df=df, table='newtable')
+    #set_pk_postgres(conn=conn, engine=engine, table='newtable', pk='ID')
+    start_time = time.time()
+    upsert_postgres(df, 'newtable', engine, colnames, 'ID')
+    print(f"Action took {(time.time() - start_time)} seconds")
+
+    conn.commit() # commit to database
+    print("Changes committed")
+    conn.close() # close connection
+    print("Connection closed")
